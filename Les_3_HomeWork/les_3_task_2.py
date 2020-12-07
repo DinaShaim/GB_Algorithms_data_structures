43da20b9ea1b0b34e0b8c3ce2f2b915a9aa273ee
ShaimD <32574729+ShaimD@users.noreply.github.com>
2020-12-08 02:54:28 +0300

    Домашняя работа к 3 уроку

---
+++ Les_3_HomeWork/les_3_task_2.py
@@ -0,0 +1,21 @@
+# Во втором массиве сохранить индексы четных элементов первого массива.
+# Например, если дан массив со значениями 8, 3, 15, 6, 4, 2,
+# второй массив надо заполнить значениями 0, 3, 4, 5, (индексация начинается с нуля),
+# т.к. именно в этих позициях первого массива стоят четные числа.
+
+import random
+
+SIZE = 10
+MIN_ITEM = 0
+MAX_ITEM = 100
+array = [random.randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
+print(array)
+
+indexes_even = []
+i = 0
+for j in range(len(array)):
+    if array[j] % 2 == 0:
+        indexes_even.insert(i, j)
+        i += 1
+
+print(f"Индексы четных чисел: {indexes_even}")
